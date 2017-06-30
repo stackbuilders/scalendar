761cbb30abe80058f1c294f3b412419d23ff21a2
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-06-30 18:31:37 -0500

    Remove unnecessary types and endpoint

--- example/src/Booking/Api.hs
+++ example/src/Booking/Api.hs
@@ -9,38 +9,29 @@ import           Data.Set      (Set)
 import           Data.Text     (Text)
 import           Servant
 
-data OneRoom = OneRoom {
-    roomId     :: Text
-  , checkInOut :: CheckInOut
-}
-
-data MultipleRooms = MultipleRooms {
-    roomIds    :: Set Text
-  , checkInOut :: CheckInOut
-}
-
 
 type BookingAPI = "hotelbooking" :>
+  -- | Return a set of availables rooms based on the CheckInOut dates
   ( "getAvailableRooms"
     :> ReqBody '[JSON] CheckInOut :> Get '[JSON] (Set Room)
   )
   :>
+  -- | Return a bolean if the given RoomId is available
   ( "isRoomAvailable"
-    :> ReqBody '[JSON] OneRoom :> Get '[JSON] Bool
+    :> ReqBody '[JSON] Text :> Get '[JSON] Bool
   )
   :>
-  ( "areRoomsAvailable"
-    :> ReqBody '[JSON] MultipleRooms :> Get '[JSON] Bool
-  )
-  :>
+  -- | Return a report set based on a set of CheckInOut dates
   ( "getPeriodicReport"
     :> ReqBody '[JSON] (Set CheckInOut) :> Get '[JSON] (Set Report)
   )
   :>
+  -- | Creates a reservation based on a Reservation data
   ( "booking"
     :> ReqBody '[JSON] Reservation :> Post '[JSON] Reservation
   )
   :>
+  -- | Removes a reservation based on the ReservationId
   ( "cancel"
     :> ReqBody '[JSON] Text :> Post '[JSON] Reservation
   )
