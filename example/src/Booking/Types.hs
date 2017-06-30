e4c0b8e462033f1386e1100236685634c3f8b584
sebashack <sebastian0092@gmail.com>
2017-06-30 01:02:04 -0500

    Setting DB action and App monad

--- example/src/Booking/Types.hs
+++ example/src/Booking/Types.hs
@@ -3,17 +3,35 @@
 
 module Booking.Types where
 
+import Data.Text (Text)
+import Control.Monad.Except
+import Control.Monad.Reader
+import Servant
+import Database.Persist.Sqlite
+import Database.Persist.Sql
+import Data.Time (UTCTime)
 import qualified Data.Set  as S
-import qualified Data.Text as T
-import qualified Data.Time as TM
 
-newtype CheckIn = MkCheckIn TM.UTCTime
+
+-- | Application type
+type App a = ReaderT ConfigDB (ExceptT ServantErr IO) a
+
+newtype ConfigDB = Config {
+    path :: Text
+  } deriving Show
+
+runAction :: SqlPersistM a -> App a -- ^ run DB actions in App context
+runAction action = reader path >>= liftIO . (flip runSqlite) action
+
+
+-- | Booking Types
+newtype CheckIn = MkCheckIn UTCTime
   deriving (Show)
 
-newtype CheckOut = MkCheckOut TM.UTCTime
+newtype CheckOut = MkCheckOut UTCTime
   deriving (Show)
 
-type Name = T.Text
+type Name = Text
 
 data Room = MkRoom {
     id   :: Integer
@@ -24,5 +42,5 @@ data Reservation = MkReservation {
     name     :: Name
   , checkIn  :: CheckIn
   , checkOut :: CheckOut
-  , rooms    :: [Room] -- sets or lists?
+  , rooms    :: [Room] -- ^ sets or lists?
 } deriving (Show)
